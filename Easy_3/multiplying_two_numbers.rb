d62069cbea20de3781ccde44c761336c2c175073
Courtnee McCollum <courtneemeaghan@gmail.com>
2021-02-19 12:44:59 -0600

    Add multiplying_two_numbers solution

---
+++ Easy_3/multiplying_two_numbers.rb
@@ -0,0 +1,31 @@
+# Create a method that takes two arguments, multiplies them together, and returns the result.
+
+# input: two integers
+# output: an integer
+
+# Test Cases:
+# input: 5, 3
+# output: 15
+# input: 10, 5
+# output: 50
+
+# Algorithm:
+# Take the 2 integer parameters and return the result of multiplying them together.
+
+# Pseudo-Code:
+
+# START
+
+# # Given 2 integers, create a method that multiplies them together and returns the result.
+
+# num1 * num2
+
+# END
+
+# -----------------------------------------------------------------------------
+
+def multiply(num1, num2)
+  num1 * num2
+end
+
+p multiply(5, 3) == 15
