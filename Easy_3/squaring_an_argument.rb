07fddc240da075753bbb0628d15ffcd3d7586fa1
Courtnee McCollum <courtneemeaghan@gmail.com>
2021-02-19 12:57:13 -0600

    Add squaring_an_argument solution

---
+++ Easy_3/squaring_an_argument.rb
@@ -0,0 +1,35 @@
+# Using the multiply method from the "Multiplying Two Numbers" problem, write a method that computes the square of its argument (the square is the result of multiplying a number by itself)
+
+# input: an integer
+# output: an integer that is the square of the input integer
+
+# Test Cases:
+# input: 5
+# output: 25
+# input: -8
+# output: 64
+
+# Algorithm:
+# Call the multiply method from within the new square method, using square's parameters as both arugments for multiply
+
+# Pseudo-Code:
+
+# START
+
+# # Given an integer, return the square of the integer using the previous problem's multiply method.
+
+# multiply(square's paramter, square's parameter)
+
+# END
+
+# -----------------------------------------------------------------------------
+
+def multiply(num1, num2)
+  num1 * num2
+end
+
+def square(num)
+  multiply(num, num)
+end
+
+p square(5) == 25
