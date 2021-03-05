019525483f47acdf846980c38c979b9ca0d7d9a6
Courtnee McCollum <courtneemeaghan@gmail.com>
2021-03-05 16:31:04 -0600

    Add multiply_lists solution

---
+++ Easy_7/multiply_lists.rb
@@ -0,0 +1,32 @@
+# input: two arrays
+# output: an array that multiplies elements from the input arrays whose index's match
+
+# Rules
+#  Explicit requirements
+#  - the input arrays will contain a list of numbers
+#  - the input arrays will have the same number of elements
+
+# Test Cases
+# multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
+
+# Algorithm
+# - initialize an empty array named results
+# - initialize an variable called counter and set equal to 0
+# - create a times loop and repeat for arr1 size amount of times
+#   - multiply arr1[counter] and arr2[counter], push to results
+#   - increment counter by 1
+# - return results
+
+def multiply_list(arr1, arr2)
+  results = []
+  counter = 0
+
+  arr1.size.times do
+    results << arr1[counter] * arr2[counter]
+    counter += 1
+  end
+
+  results
+end
+
+p multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
